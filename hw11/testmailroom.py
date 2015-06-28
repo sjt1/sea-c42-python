7204edc5f13a3dca771e5f0f6d653b373c5f61d3
Spencer Tower <spencerjtower1@gmail.com>
2015-06-28 13:18:30 -0700

    Change variable a to user_input.

--- hw11/testmailroom.py
+++ hw11/testmailroom.py
@@ -1,69 +1,74 @@
+
+
 def main_menu():
+    '''Print main menu options to terminal and take user input.'''
 
-    a = input('''Choose from the following:
-
+    user_input = input('''Choose from the following:
 T - Send a (T)hank You
 
 R - Create a Report
 
 quit - Quit the program
 
->''')
+> ''')
 
-    if (a == "T"):
-        print("thankYou()")
+    if (user_input == "T"):
+        thank_you_menu()
 
-    elif (a == "R"):
-        print("report()")
+    elif (user_input == "R"):
+        report()
 
-    elif (a == "quit"):
+    elif (user_input == "quit"):
         exit()
+
+    else:
+        main_menu()  # make it do nothing instead
 
 
 def thank_you_menu():
-    a = ('''Please enter a name or choose from the following:
+    '''Print thank you options to terminal and take user input.'''
+
+    user_input = input('''Please enter a name or choose from the following:
 
 list - Print a list of previous donors
 
-quit - Return to main menu''')
+quit - Return to main menu
 
-    if (a == "list"):
-        print(donor_list)
+> ''')
 
-    elif (a == quit):
+    if (user_input == "list"):
+        for name in donor_list:
+            print(name)
+
+        print("")    # Adds a blank line
+
+        thank_you_menu()
+
+    elif (user_input == "quit"):
         main_menu()
 
-    elif (a == ""):
+    elif (user_input == ""):
         x = input("""Please enter a donation amount or 'quit':
 
 > """)
-        addName(x)
+        donation(user_input)
 
 
+donor_list = ["Spencer Tower", "Bill Gates"]    # List of donors
+
+donation_list = [100, 100]    # List of donations
 
 
+def donation(amount):
+    if (amount == int or float):
+        donation_list.append(amount)
+        print(donation_list)
 
 
+def donors(name):
+    donors.append(name)
 
 
+print("Welcome to Mailroom Madness!\n")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-__main__
-
-print("Welcome to Mailroom Madness!")  # make new line after
-
-print(main_prompt())
+print(main_menu())
