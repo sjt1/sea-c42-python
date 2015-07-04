9c34bb7072ebcb7f8f5e7b55e2140942c400d91c
Spencer Tower <spencerjtower1@gmail.com>
2015-07-03 19:42:40 -0700

    Save changes to testmailroom.py.

--- hw11/testmailroom.py
+++ hw11/testmailroom.py
@@ -1,7 +1,6 @@
 # Spencer Tower
 # sea-c42-py
 # Mailroom Madness
-from operator import itemgetter
 
 
 donor_list = [['Spencer Tower', 20, 5, 10], ['Bill Gates', 100, 1000, 30],
@@ -11,21 +10,16 @@ donor_list = [['Spencer Tower', 20, 5, 10], ['Bill Gates', 100, 1000, 30],
 
 def main_menu():
     '''Print main menu options to terminal and take user input.'''
-    user_input = input('''Choose from the following:
-T - Send a (T)hank You
-R - Create a Report
-quit - Quit the program
-> ''')
+    user_input = input('''Choose from the following:\nT - Send a (T)hank You
+R - Create a (R)eport\nquit - Quit the program\n> ''')
 
     if (user_input == "T"):
         thank_you_menu()
-
     elif (user_input == "R"):
         report()
-
+        main_menu()
     elif (user_input == "quit"):
         exit()
-
     else:
         main_menu()  # make it do nothing instead
 
@@ -33,36 +27,19 @@ quit - Quit the program
 def thank_you_menu():
     '''Print thank you options to terminal and take user input.'''
     user_input = input('''Please enter a name or choose from the following:
-list - Print a list of previous donors
-quit - Return to main menu
-> ''')
+list - Print a list of previous donors\nquit - Return to main menu\n> ''')
 
     if (user_input == "list"):
         for name in donor_list:
             for i in name:
-                return(name[0])
+                print(name[0])
                 break
         thank_you_menu()
-
-    elif (user_input in donor_list):
-        x = input("""Please enter a donation amount or 'quit':
-
-> """)
-        donation(int(x))
-        print_canned_letter(user_input, x)
-
-    elif (user_input not in donor_list):
-        donor_list.append([user_input])
-        return(donor_list)
-        x = input("""Please enter a donation amount or 'quit':
-
-> """)
-        donation(float(x))
-        print_canned_letter(user_input, x)
-
     elif (user_input == "quit"):
-
         main_menu()
+    else:
+        append_donation(user_input)
+        thank_you_menu()
 
 
 def report():
@@ -71,22 +48,35 @@ def report():
         x = 0
         for i in donor[1:]:
             x = x + i
-        print("%s | %s | %s " % (donor[0], x, len(donor[1:]), x / len(donor[1:])))
+        print("%s | %d | %d | %d" % (donor[0], x, len(donor[1:]), x / len(donor[1:])))
 
 
-def donation(amount):
-    if (amount == int or float):
-        donor_list.append(float(amount))
+def append_donation(name):
+    found = False
+    for donor in donor_list:
+        if name == donor[0]
+                found = True
+                amount = input("Please enter a donation amount.\n> ")
+                donor.append(float(amount))
+                print(donor_list)
+                break
+    if (not found):
+        for donor in donor_list:
+            if name == donor[0]:
+                if d[0] != name:
+                    donor_list.append([name])
+                    amount = input("Please enter a donation amount.\n> ")
+                    donor_list[-1].append(float(amount))
+                    print(donor_list)
 
 
-def print_canned_letter(donor, amount):
-    return('''Dear %s,
+def canned_letter(donor, amount):
+    print('''Dear %s,
 Thank you so much for your kind donation of $%s!  We here at The Foundation
 for Capeless Super Heroes appreciate your generosity.  Your money will
 go towards ensuring that no super hero goes another night capeless and
-each one can continue to fight crime with dignity and stay super.''' % (donor, float(amount)))
+each one can continue to fight crime with dignity and stay super.\n''' % (donor, float(amount)))
 
-    thank_you_menu()
 
 print("Welcome to Mailroom Madness!\n")
 
