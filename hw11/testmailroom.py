a8c65e1a9af4db87d7a6908c608f28c8b5e8fd95
Spencer Tower <spencerjtower1@gmail.com>
2015-07-03 20:16:01 -0700

    Add improvements to append_donation.

--- hw11/testmailroom.py
+++ hw11/testmailroom.py
@@ -3,9 +3,9 @@
 # Mailroom Madness
 
 
-donor_list = [['Spencer Tower', 20, 5, 10], ['Bill Gates', 100, 1000, 30],
-              ['Incredible Hulk', 20, 15, 70], ['Gandalf', 1, 400, 4],
-              ['Smeagle', 10, 40, 50]]
+donor_list = [['Spencer Tower', [20, 5, 10]], ['Bill Gates', [100, 1000, 30]],
+              ['Incredible Hulk', [20, 15, 70]], ['Gandalf', [1, 400, 4]],
+              ['Smeagle', [10, 40, 50]]]
 
 
 def main_menu():
@@ -39,35 +39,29 @@ list - Print a list of previous donors\nquit - Return to main menu\n> ''')
         main_menu()
     else:
         append_donation(user_input)
-        thank_you_menu()
 
 
 def report():
-    print('Name      |    Total   |    #   |    Average')
+    print('Name  \t\tTotal  \t\t#  \t\tAverage')
     for donor in donor_list:
         x = 0
         for i in donor[1:]:
             x = x + i
-        print("%s | %d | %d | %d" % (donor[0], x, len(donor[1:]), x / len(donor[1:])))
+        print("%s  \t\t%d  \t\t%d  \t\t%d" % (donor[0], x, len(donor[1:]), x / len(donor[1:])))
 
 
 def append_donation(name):
-    found = False
     for donor in donor_list:
-        if name == donor[0]
-                found = True
-                amount = input("Please enter a donation amount.\n> ")
-                donor.append(float(amount))
-                print(donor_list)
-                break
-    if (not found):
-        for donor in donor_list:
-            if name == donor[0]:
-                if d[0] != name:
-                    donor_list.append([name])
-                    amount = input("Please enter a donation amount.\n> ")
-                    donor_list[-1].append(float(amount))
-                    print(donor_list)
+        if (name == donor[0]):
+            amount = input("Please enter a donation amount.\n> ")
+            donor_list[donor][1].append(float(amount))
+            print(canned_letter(name, amount))
+            break
+        else:
+            donor_list.append([name, []])
+            amount = input("Please enter a donation amount.\n> ")
+            donor_list[-1][1].append(float(amount))
+            print(canned_letter(name, amount))
 
 
 def canned_letter(donor, amount):
