1bc5bf5d90013421aacc058c85c7c107e1e4a331
Spencer Tower <spencerjtower1@gmail.com>
2015-07-07 16:34:46 -0700

    Complete mailroom.py. Needs some adjustments but does what its supposed to do. Delete other file in dir.

--- hw11/mailroom.py
+++ hw11/mailroom.py
@@ -1,106 +1,82 @@
+# Spencer Tower
+# sea-c42-py
+# Mailroom Madness
+
+
+donor_list = [['Spencer Tower', 20, 5, 10], ['Bill Gates', 100, 1000, 30],
+              ['Incredible Hulk', 20, 15, 70], ['Gandalf', 1, 400, 4],
+              ['Smeagle', 10, 40, 50]]
 
 
 def main_menu():
-    a = input("Choose from the following:", "T - Send a (T)hank You",
-        "R - Create a Report", "quit - Quit the program")
+    '''Print main menu options to terminal and take user input.'''
+    user_input = input('''Choose from the following:\n
+                        T - Send a (T)hank You\n
+                        R - Create a (R)eport\n
+                        quit - Quit the program\n> ''')
 
-    if (a == "T"):
-        thankYou()
-
-    elif (a == "R"):
-        report()
-
-    elif (a == "quit"):
-        quitProgram()
-
-'''
-def report(): ***
-    generate a list for each donor *Sorted by total donation amount*:
-Full Name, Total, Number of Donations
-    traverse list of donors and maps their names to their donation lists.
-Sum their list of donations and print out their info starting with
-donor who has the highest sum.
-'''
-
-def thankYou():
-    a = input("Please enter a name, or choose from the following:",
-        "list - Print a list of previous donors", "quit - Return to main menu")
-
-    if (a == "list"):
-        print(donors)
-
-    elif (a == "quit"):
-        mainPrompt()
-
-
-'''
-    elif (a == ""):
-        Check that the name is in the database - iterate over names.
-
-        if (name is in database):
-            prompt user to enter an amount
-            call function numberVerify()
-
-        if (name is not in database):
-            add name to database
-            and prompt user to enter amount
-            call function numberVerify()
-'''
-'''
-
-donors = ["Spencer Tower", "Bill Gates", "Incredible Hulk", "Gandalf",
-"Smeagle"]
-
-donations = [[100], [50]]
-
-def donorList()
-print(donors)
-
-for i in donors:
-
-    if a == a donor name
-    print("Please enter a donation amount or 'quit'")
-
-
-print("""Dear %s,
-    Thank you so much for you kind donation of %s.  We here at the
-    foundation for Super Heros in Need of Capes greatly appreciate your
-    generosity!  Your money will go to ensuring that no superhero goes
-    capeless and each can continue to fight crime with dignity.""") % donor, donation
-
-
-
-
-donors.append()
-donations.append() top list
-donations.append() nested lists
-
-
-
-
-
-def quitProgram():
-    quit the program
-
-
-def numberVerify(n):
-    if (n != float or int):
-        call the function thankYouMenu()
-
+    if (user_input == "T"):
+        thank_you_menu()
+    elif (user_input == "R"):
+        create_report()
+        main_menu()
+    elif (user_input == "quit"):
+        exit()
     else:
-        call the function sendThankYou()
+        main_menu()
 
 
-def sendThankYou(fullName, amount):
-    sub fullName and amount into placeholders of canned thank you letter.
-    Print Thank You letter to terminal.
+def thank_you_menu():
+    '''Print thank you options to terminal and take user input.'''
+    user_input = input('''Please enter a name or choose from the following:\n
+                        list - Print a list of previous donors\n
+                        quit - Return to main menu\n> ''')
 
-def list():
-    a for loop that iterates over the list of donor names and returns
-    them in alphabetical order.
-'''
+    if (user_input == "list"):
+        for name in donor_list:
+            for i in name:
+                print(name[0])
+                break
+        thank_you_menu()
+    elif (user_input == "quit"):
+        main_menu()
+    else:
+        append_donation(user_input)
+        thank_you_menu()
 
 
-print("Welcome to Mailroom Madness!")
+def create_report():
+    print('\tName\t\tTotal\t\t#\tAverage')
+    for donor in donor_list:
+        x = 0
+        for i in donor[1:]:
+            x = x + i
+        print('%s\t\t$%d\t\t%d\t$%d' % (donor[0], x, len(donor[1:]), x / len(donor[1:])))
 
-main_menu()
+
+def append_donation(name):
+    found = False
+    for donor in donor_list:
+        if (name == donor[0]):
+            amount = float(input("Please enter a donation amount.\n> "))
+            donor.append(amount)
+            print(canned_letter(name, amount))
+            found = True
+            break
+    if (not found):
+        amount = float(input("Please enter a donation amount.\n> "))
+        donor_list.append([name, amount])
+        print(canned_letter(name, amount))
+
+
+def canned_letter(donor, amount):
+    return('''Dear %s,
+Thank you so much for your kind donation of $%s!  We here at The Foundation
+for Capeless Super Heroes appreciate your generosity.  Your money will
+go towards ensuring that no super hero goes another night capeless and
+each one can continue to fight crime with dignity and stay super.\n''' % (donor, float(amount)))
+
+
+print("Welcome to Mailroom Madness!\n")
+
+print(main_menu())
